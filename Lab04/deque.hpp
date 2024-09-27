7338b43c9c4784351acba5afeadbe4a045df86dd
kholcomb914 <k1t.holcomb@outlook.com>
2024-09-26 22:21:47 -0500

    deque.hpp

--- Lab04/deque.hpp
+++ Lab04/deque.hpp
@@ -1,93 +1,41 @@
 //
-//  deque.cpp
+//  deque.hpp
 //  Lab04
 //
 //  Created by Kaustubh on 9/25/24.
 //
 
-#include "deque.hpp"
-#include <iostream>
+#ifndef deque_hpp
+#define deque_hpp
 
-Deque::Node::Node(int cVal, Node *cPrevNode, Node *cNextNode) {
-    val = cVal;
-    prevNode = cPrevNode;
-    nextNode = cNextNode;
-}
-Deque::Deque() {
-    topOfDeque = nullptr;
-    botOfDeque = nullptr;
-}
+#include <stdio.h>
 
-Deque::Node *Deque::getTopOfDeque() {
-    return topOfDeque;
-}
-Deque::Node *Deque::getBotOfDeque() {
-    return botOfDeque;
-}
-void Deque::setTopOfDeque(Node *n) {
-    topOfDeque = n;
-}
-void Deque::setBotOfDeque(Node *n) {
-    botOfDeque = n;
-}
+class Deque {
 
-void Deque::push_back(int i) {
-    Node *newNode = new Node(i, botOfDeque, topOfDeque);
-    setBotOfDeque(newNode);
-}
-void Deque::push_front(int i) {
-    Node *newNode = new Node(i, botOfDeque, topOfDeque);
-    setTopOfDeque(newNode);
-}
+private:
+    struct Node {
+        int val;
+        Node *nextNode;
+        Node *prevNode;
+        Node(int cVal, Node *cPrevNode, Node *cNextNode);
+    };
+    Node *topOfDeque;
+    Node *botOfDeque;
 
-int Deque::pop_back() {
-    if(topOfDeque != nullptr && botOfDeque != nullptr) {
-        int val = botOfDeque -> val;
-        Node *ptrNewBot = botOfDeque -> prevNode;
-        delete botOfDeque;
-        setBotOfDeque(ptrNewBot);
-        return val;
-    }
-    std::cout << "Deque is empty--operation pop_back cannot be executed." << std::endl;
-    return 0;
-}
-int Deque::pop_front() {
-    if(topOfDeque != nullptr && botOfDeque != nullptr) {
-        int val = topOfDeque -> val;
-        Node *ptrNewTop = topOfDeque -> nextNode;
-        delete topOfDeque;
-        setTopOfDeque(ptrNewTop);
-        return val;
-    }
-    std::cout << "Deque is empty--operation pop_front cannot be executed." << std::endl;
-    return 0;
-}
+public:
+    Deque();
+    Node *getTopOfDeque();
+    Node *getBotOfDeque();
+    void setTopOfDeque(Node *n);
+    void setBotOfDeque(Node *n);
+    void push_front(int);
+    void push_back(int);
+    int pop_front();
+    int pop_back();
+    int peak_front();
+    int peak_back();
+    void remove_all();
+    ~Deque();
+};
 
-int Deque::peak_back() {
-    if(topOfDeque != nullptr && botOfDeque != nullptr) {
-        int val = botOfDeque -> val;
-        return val;
-    }
-    std::cout << "Deque is empty--operation peak_back cannot be executed." << std::endl;
-    return 0;
-}
-int Deque::peak_front() {
-    if(topOfDeque != nullptr && botOfDeque != nullptr) {
-        int val = topOfDeque -> val;
-        return val;
-    }
-    std::cout << "Deque is empty--operation peak_front cannot be executed." << std::endl;
-    return 0;
-}
-
-void Deque::remove_all() {
-    while(getTopOfDeque() != nullptr) {
-        Node *ptrNewTop = topOfDeque -> nextNode;
-        delete topOfDeque;
-        setTopOfDeque(ptrNewTop);
-    }
-}
-
-Deque::~Deque() {
-    remove_all();
-}
+#endif /* deque_hpp */
